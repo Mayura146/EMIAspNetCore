b2f26c8ed4ed13af82e7cd7db7ebe00e91c3943c
Mayura-146 <mayura.sonar@outlook.com>
2020-09-22 11:03:02 +0530

    candyshop

---
+++ CandyOnlineShopping/CandyOnlineShopping/Controllers/CandyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyOnlineShopping.Models.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CandyOnlineShopping.Controllers
+{
+    public class CandyController : Controller
+    {
+        private readonly ICandyService _candyService;
+        private readonly ICategoryService _categoryService;
+
+        public CandyController(ICandyService candyService,ICategoryService categoryService)
+        {
+            _candyService = candyService;
+            _categoryService = categoryService;
+        }
+        public IActionResult List()
+        {
+            ViewBag.CurrentCategory = "Best Sellers!!!";
+            return View(_candyService.GetAll());
+        }
+    }
+}
---
+++ CandyOnlineShopping/CandyOnlineShopping/Models/Repositories/CandyRepository.cs
@@ -0,0 +1,43 @@
+﻿using CandyOnlineShopping.Models.Entity;
+using CandyOnlineShopping.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyOnlineShopping.Models.Repositories
+{
+    public class CandyRepository : ICandyRepsoitory
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        public CandyRepository(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public IEnumerable<Candy> GetAll()
+        {
+            List<Candy> candies = new List<Candy>()
+                {
+                    new Candy{Id=1,Name="Assorted Hard Candy",Price=45.4M,Description="Assorted Hardy Candy",Category=_categoryRepository.GetAll().ToList()[0],
+                        ImageUrl="\\Images\\hardCandy.jpg",IsInStock=true,IsOnSale=false,ImageThumbnailUrl="\\Images\\thumbnail\\hardCandy-small.jpg"
+                },
+
+                    new Candy { Id=2,Name="Assorted Chocalate Candy",Price=5.95M,Description="Assorted Chocalte Candy",Category=_categoryRepository.GetAll().ToList()[1],ImageUrl="\\Images\\chocolateCandy.jpg",IsInStock=true,IsOnSale=false,ImageThumbnailUrl= "\\Images\\thumbnail\\chocolateCandy-small.jpg" },
+
+                     new Candy { Id=2,Name="Assorted Fruit Candy",Price=3.95M,Description="Assorted Fruit Candy",Category=_categoryRepository.GetAll().ToList()[2],ImageUrl="\\Images\\FruitCandy.jpg",IsInStock=true,IsOnSale=false,ImageThumbnailUrl= "\\Images\\thumbnail\\FruitCandy-small.jpg" }
+            };
+            return candies;
+        }
+
+        public Candy GetById(int candyId)
+        {
+            return GetAll().FirstOrDefault(c => c.Id == candyId);
+            
+        }
+
+        public IEnumerable<Candy> GetOnSale()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
