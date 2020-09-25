7329cc102e962ac8cf8615ee3533bc2c1bd8741b
Mayura-146 <mayura.sonar@outlook.com>
2020-09-26 03:41:26 +0530

    RefactoredShoppingCartcode

--- CandyOnlineShopping/CandyOnlineShopping/Models/Repositories/ShoppingCartItemRepository.cs
+++ CandyOnlineShopping/CandyOnlineShopping/Models/Repositories/ShoppingCartItemRepository.cs
@@ -2,6 +2,7 @@
 using CandyOnlineShopping.Models.Entity;
 using CandyOnlineShopping.Models.Interfaces;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -10,52 +11,85 @@ using System.Threading.Tasks;
 
 namespace CandyOnlineShopping.Models.Repositories
 {
-    //public class ShoppingCartItemRepository : IShoppingCartRepository
-    //{
-    //    private AppDbContext _appDbContext;
+    public class ShoppingCartRepository : ShoppingCartItem, IShoppingCartRepository
+    {
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+        private readonly AppDbContext _appDbContext;
 
-    //    public ShoppingCartItemRepository(AppDbContext appDbContext)
-    //    {
-    //        _appDbContext = appDbContext;
-    //    }
-    //    public void AddToCart(Candy candy, int amount)
-    //    {
-    //        var shoppingCartItem = _appDbContext.ShoppingCartItem.
-    //            SingleOrDefault(s => s.Candy.Id == candy.Id && );
-    //    }
+        public ShoppingCartRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public void AddToCart(Candy candy, int amount)
+        {
+            var shoppingcartitem = _appDbContext.ShoppingCartItem.
+                  SingleOrDefault(s => s.Candy.Id == candy.Id && s.ShoppingCartId == ShoppingCartId);
 
-    //    public void ClearCart()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+            if (shoppingcartitem == null)
+            {
+                shoppingcartitem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Candy = candy,
+                    Amount = amount
+                };
+                _appDbContext.ShoppingCartItem.Add(shoppingcartitem);
+            }
 
-    //    public static ShoppingCart GetCart(IServiceProvider serviceProvider)
-    //    {
-    //        ISession session = serviceProvider.GetRequiredService<IHttpContextAccessor>()
-    //            ?.HttpContext.Session;
-    //        var context = serviceProvider.GetService<AppDbContext>();
-    //        string cartId = session.GetString("Id") ?? Guid.NewGuid().ToString();
-    //        session.SetString("Id", cartId);
-    //        return new ShoppingCart(context)
-    //        {
-    //            Id = cartId
-    //        };
+            else
+            {
+                // increment of u find item in cart
 
-    //    }
+                shoppingcartitem.Amount++;
+            }
+            _appDbContext.SaveChanges();
+        }
 
-    //    public List<ShoppingCartItem> GetShoppingCartItems()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        public void ClearCart()
+        {
+            var cartitems = _appDbContext.ShoppingCartItem.
+                  Where(c => c.ShoppingCartId == ShoppingCartId);
+            _appDbContext.ShoppingCartItem.RemoveRange(cartitems);
+            _appDbContext.SaveChanges();
+        }
 
-    //    public decimal GetShoppingCartTotal()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _appDbContext.ShoppingCartItem.
+               Where(c => c.ShoppingCartId == ShoppingCartId).Include(s => s.Candy).ToList());
+        }
 
-    //    public int RemoveFromCart(Candy candy)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
+        public decimal GetShoppingCartTotal()
+        {
+            var total = _appDbContext.ShoppingCartItem.
+                 Where(c => c.ShoppingCartId == ShoppingCartId).
+                 Select(c => c.Candy.Price * c.Amount).Sum();
+            return total;
+        }
+
+        public int RemoveFromCart(Candy candy)
+        {
+            var shoppingCartItem = _appDbContext.ShoppingCartItem.
+           SingleOrDefault(s => s.Candy.Id == candy.Id && s.ShoppingCartId == ShoppingCartId);
+            var localAmount = 0;
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                    localAmount = shoppingCartItem.Amount;
+                }
+
+                else
+                {
+                    _appDbContext.ShoppingCartItem.Remove(shoppingCartItem);
+                }
+            }
+
+            _appDbContext.SaveChanges();
+            return localAmount;
+        }
+
+
     }
-//}
+}
---
+++ CandyOnlineShopping/CandyOnlineShopping/Models/Services/IShoppingCartService.cs
@@ -0,0 +1,18 @@
+﻿using CandyOnlineShopping.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyOnlineShopping.Models.Services
+{
+    interface IShoppingCartService
+    {
+        void AddToCart(Candy candy, int amount);
+        int RemoveFromCart(Candy candy);
+        IEnumerable<ShoppingCartItem> GetShoppingCartItems();
+
+        decimal GetShoppingCartTotal();
+        void ClearCart();
+    }
+}
